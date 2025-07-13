01594a9f4ee8266b610a0361cd07e4f6907bf652
lyh990517 <lyh990517@naver.com>
2025-07-13 23:11:15 +0900

    change indicator size

--- ui/pull-to-refresh-with-nested-scroll/src/main/java/com/yunho/pull/to/refresh/RefreshIndicatorUi.kt
+++ ui/pull-to-refresh-with-nested-scroll/src/main/java/com/yunho/pull/to/refresh/RefreshIndicatorUi.kt
@@ -3,9 +3,11 @@ package com.yunho.pull.to.refresh
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.yunho.common.Lottie
 import com.yunho.common.R
 
@@ -19,6 +21,9 @@ fun RefreshIndicator(
             .height(refreshIndicator.value),
         contentAlignment = Alignment.Center
     ) {
-        Lottie(animId = R.raw.anim_loading)
+        Lottie(
+            modifier = Modifier.size(32.dp),
+            animId = R.raw.anim_loading
+        )
     }
 }
